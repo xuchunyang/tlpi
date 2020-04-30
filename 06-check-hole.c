3bca0dbc4624c675aca444defb35f02c80ac4ae2
Xu Chunyang <xuchunyang56@gmail.com>
2020-04-30 12:50:56 +0800

    * 06-check-hole.c (main): Check if _PC_MIN_HOLE_SIZE available.

--- 06-check-hole.c
+++ 06-check-hole.c
@@ -6,6 +6,7 @@
 int
 main()
 {
+#ifdef _PC_MIN_HOLE_SIZE
     long size = pathconf("Makefile", _PC_MIN_HOLE_SIZE);
     if (size == -1) {
         perror("pathconf(_PC_MIN_HOLE_SIZE)");
@@ -13,4 +14,7 @@ main()
     }
     /* 文件系统支持 hole？ */
     printf("_PC_MIN_HOLE_SIZE: %ld\n", size);
+#else
+    printf("_PC_MIN_HOLE_SIZE 没有定义，它只在 BSD 系统上才有定义，Linux 上没有\n");
+#endif
 }
