9ca23c6f19a1122ccf445cf1fcbc57ca1a03ad0d
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-01 00:51:22 +0800

    * 22-mem.c: Update.

--- 22-mem.c
+++ 22-mem.c
@@ -8,4 +8,10 @@ main()
     extern char etext, edata, end;
     printf("%d\n", etext);
 #endif
+
+    /* ex6-1 */
+    static char mbuf[10240000] = {0}; /* 4.2k 没初始化的数据段，几乎不占空间 */
+    // static char mbuf[10240000] = {1}; /* 10MB 初始化的数据段，占空间 */
+
+    (void) mbuf;
 }
