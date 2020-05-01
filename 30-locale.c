b003d6cfbfe22cb5e81f865c6ba9ba0e5a5b6dba
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-01 20:59:10 +0800

    * 30-locale.c: New.

---
+++ 30-locale.c
@@ -0,0 +1,34 @@
+/* 30-locale.c --- locale */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <locale.h>
+
+int
+main()
+{
+    time_t t = time(0);
+    struct tm *tt = localtime(&t);
+    if (tt == NULL) {
+        perror("localtime");
+        exit(EXIT_FAILURE);
+    }
+
+#define BUF_SIZE 1024
+
+    char buf[BUF_SIZE];
+    assert(strftime(buf, BUF_SIZE,
+                    "%H:%M:%S %A, %d %B %Y %Z", tt));
+    printf("'%s'\n", buf);
+
+    /* 继承环境变量 */
+    if (setlocale(LC_ALL, "") == NULL) {
+        printf("setlocale failed\n");
+        exit(EXIT_FAILURE);
+    }
+    assert(strftime(buf, BUF_SIZE,
+                    "%H:%M:%S %A, %d %B %Y %Z", tt));
+    printf("'%s'\n", buf);
+}
