9597064a760d50ebf69dade6fbc693d0be898b91
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-02 02:18:15 +0000

    Fix linux build

--- 32-limits.c
+++ 32-limits.c
@@ -25,13 +25,11 @@ main()
 
     printf("_SC_NGROUPS_MAX: %ld\n", sysconf(_SC_NGROUPS_MAX));
 
-    printf("ARG_MAX: %d\n", ARG_MAX);
     printf("_SC_ARG_MAX: %ld\n", sysconf(_SC_ARG_MAX));
 
     // printf("%d\n", LOGIN_NAME_MAX);
     printf("_SC_LOGIN_NAME_MAX: %ld\n", sysconf(_SC_LOGIN_NAME_MAX));
 
-    printf("OPEN_MAX: %d\n", OPEN_MAX);
     printf("_SC_OPEN_MAX: %ld\n", sysconf(_SC_OPEN_MAX));
 
     printf("PATH_MAX: %d\n", PATH_MAX);
