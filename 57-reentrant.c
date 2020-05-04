82a9cd053cb15751c6705d7a182634238c0167fa
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-04 11:13:09 +0800

    * 57-reentrant.c: New.

---
+++ 57-reentrant.c
@@ -0,0 +1,33 @@
+/* 57-reentrant.c --- 可重入 */
+
+/* crypt(3) 使用静态分配变量，不可重入 */
+
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+static int handled = 0;
+
+static void
+handler(int sig)
+{
+    handled++;
+    crypt("def", "xx");
+}
+
+int
+main()
+{
+    signal(SIGINT, handler);
+    char* cr1 = strdup(crypt("abc", "xx"));
+    int callNum = 0;
+    int mismatched = 0;
+    for(;;callNum++) {
+        if (strcmp(crypt("abc", "xx"), cr1) != 0) {
+            mismatched++;
+            printf("total = %d, handled = %d, mismatched = %d\n",
+                   callNum, handled, mismatched);
+        }
+    }
+}
