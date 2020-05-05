b462a0f5a4da0dc449ebedc9441ba922e0c500b8
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-06 03:21:28 +0800

    * 68-atexit.c: New.

---
+++ 68-atexit.c
@@ -0,0 +1,62 @@
+/* 68-atexit.c --- atexit */
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static void
+f1()
+{
+    puts("calling f1()");
+}
+
+static void
+f2()
+{
+    puts("calling f2()");
+}
+
+static void
+f3()
+{
+    puts("calling f3()");
+}
+
+
+int
+main(int argc, char* argv[])
+{
+    if (argc >= 2 && strcmp(argv[1], "--help") == 0) {
+        printf("usage: %s [_exit] [sig]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (atexit(f1) == -1) {
+        perror("atexit");
+        exit(EXIT_FAILURE);
+    }
+
+    if (atexit(f2) == -1) {
+        perror("atexit");
+        exit(EXIT_FAILURE);
+    }
+
+    if (atexit(f3) == -1) {
+        perror("atexit");
+        exit(EXIT_FAILURE);
+    }
+
+    switch (argc) {
+    case 1:
+        puts("normal exit");
+        exit(EXIT_SUCCESS);
+    case 2:
+        puts("abnormal exit via _exit()");
+        _exit(EXIT_SUCCESS);
+    case 3:
+        puts("abnormal exit via sig");
+        raise(SIGINT);
+        sleep(3);
+    }
+}
