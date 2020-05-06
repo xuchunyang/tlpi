744ec890bd753a15aca7cea2119c0f5b7bdc6c42
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-06 19:45:23 +0800

    * 75-SIGCHILD.c: New.

---
+++ 75-SIGCHILD.c
@@ -0,0 +1,34 @@
+/* 75-SIGCHILD.c --- SIGHILD */
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void
+handler(int sig)
+{
+    psignal(sig, 0);
+}
+
+int
+main()
+{
+    if (signal(SIGCHLD, handler) == SIG_ERR) {
+        perror("signal");
+        exit(EXIT_FAILURE);
+    }
+
+#define NP 3
+
+    for (int i = 0; i < NP; i++)
+        switch (fork()) {
+        case -1:
+            perror("fork");
+            exit(EXIT_FAILURE);
+        case 0:
+            _exit(EXIT_FAILURE);
+        }
+
+    for(int i = 0; i < NP; i++)
+        pause();
+}
