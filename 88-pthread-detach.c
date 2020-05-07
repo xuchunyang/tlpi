164aabd0a8fceea804b013b4bd7d91c9ac9a3087
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-07 22:57:54 +0800

    * 88-pthread-detach.c: New.

--- 88-pthread-detach.c
+++ 88-pthread-detach.c
@@ -1,13 +1,27 @@
-/* 88-pthread-detach.c --- joinable vs detch  */
+/* 88-pthread-detach.c --- ex29-1 joinable vs detch  */
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 
 static void*
 thread_start(void *arg)
 {
+    int status = pthread_join(pthread_self(), NULL);
+    switch (status) {
+    case 0:
+      break;
+
+    case EINVAL:
+      printf("pthread_join: thread is not joinable\n");
+      break;
+
+    default:
+      printf("pthread_join: %s\n", strerror(status));
+      break;
+    }
     return arg;
 }
 
@@ -25,9 +39,10 @@ main()
     int status = pthread_create(&thread, NULL, thread_start, NULL);
     if (status != 0) errExit(status, "pthread_create");
 
-    status = pthread_detach(thread);
-    if (status != 0) errExit(status, "pthread_detach");
+    /* status = pthread_detach(thread); */
+    /* if (status != 0) errExit(status, "pthread_detach"); */
 
+    sleep(1);
     status = pthread_join(thread, NULL);
     if (status != 0) {
         if (status == EINVAL) {
