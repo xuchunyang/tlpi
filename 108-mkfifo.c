daf56491c9d89d8474d37967446b5021cfce16d7
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-09 22:00:07 +0800

    * 108-mkfifo.c (main): Detect EXISTS case.

--- 108-mkfifo.c
+++ 108-mkfifo.c
@@ -1,4 +1,5 @@
 /* 108-mkfifo.c --- create FIFO (named pipe) file with mkfifo(2) */
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -7,7 +8,16 @@ int
 main()
 {
     if (mkfifo("fifo", S_IRUSR | S_IWUSR) == -1) {
-        perror("mkfifo");
-        exit(EXIT_FAILURE);
+        if (errno == EEXIST) {
+            printf("fifo exists\n");
+            exit(EXIT_SUCCESS);
+        }
+        else {
+            perror("mkfifo");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        printf("fifo created\n");
+        exit(EXIT_SUCCESS);
     }
 }
