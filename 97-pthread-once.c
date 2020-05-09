acc2b1850d9786fb2faac427f5dc9c48deadea3c
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-09 10:52:46 +0800

    * 97-pthread-once.c: New.

---
+++ 97-pthread-once.c
@@ -0,0 +1,36 @@
+/* 97-pthread-once.c --- pthread_once */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+pthread_once_t once = PTHREAD_ONCE_INIT;
+
+static void
+setup()
+{
+    printf("setup run only once no matter how many time it is called\n");
+}
+
+static void*
+thread_start(void* arg)
+{
+    printf("thread_start\n");
+    pthread_once(&once, setup);
+}
+
+int
+main()
+{
+    for (int i = 0; i < 10; i++) {
+        pthread_t thread;
+        int status = pthread_create(&thread, NULL, thread_start, NULL);
+        if (status != 0) {
+            printf("pthread_create: %s\n", strerror(status));
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    pthread_exit(NULL);
+}
