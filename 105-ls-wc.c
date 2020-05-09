62af5664e1564c07f2d6780e010653f896374085
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-09 19:33:10 +0800

    * 105-ls-wc.c: 防御性编程实践.

--- 105-ls-wc.c
+++ 105-ls-wc.c
@@ -25,10 +25,15 @@ main()
         if (close(fds[0]))      /* close read */
             errExit("close");
 
-        if (dup2(fds[1], STDOUT_FILENO) == -1)
-            errExit("dup2");
+        if (fds[1] != STDOUT_FILENO) { /* p737, 防御性 */
+            if (dup2(fds[1], STDOUT_FILENO) == -1)
+                errExit("dup2");
+            if (close(fds[1]) == -1) /* 关闭 solo fd */
+                errExit("close");
+        }
 
         execlp("ls", "ls", NULL);
+        errExit("execlp ls");
         /* NOTE 结果是对的，但底气不足。wc 进程应该是会等待 ls 进程结束的，这一点无疑 */
         // execlp("find", "find", ".", NULL);
         // execlp("yes", "yes", ".", NULL);
@@ -36,9 +41,14 @@ main()
         if (close(fds[1]))      /* close write */
             errExit("close");
 
-        if (dup2(fds[0], STDIN_FILENO) == -1)
-            errExit("dup2");
+        if (fds[0] != STDIN_FILENO) {
+            if (dup2(fds[0], STDIN_FILENO) == -1)
+                errExit("dup2");
+            if (close(fds[0]) == -1)
+                errExit("close");
+        }
 
         execlp("wc", "wc", "-l", NULL);
+        errExit("execlp wc");
     }
 }
