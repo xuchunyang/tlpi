e4a7c89b0f09f74fa15f7d224892e163dc78129a
Xu Chunyang <xuchunyang56@gmail.com>
2020-05-12 17:52:07 +0800

    * 125-pseudo-terminal.c: Fix include.

--- 125-pseudo-terminal.c
+++ 125-pseudo-terminal.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <sys/_select.h>
-#include <sys/fcntl.h>
 #include <sys/ioctl.h>
 #include <sys/termios.h>
 #include <sys/ttycom.h>
